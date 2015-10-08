b70cf4fc2ac7458e3e726aa55f73d1670d7ba325
Kristy Caster <kristyacaster@gmail.com>
2015-10-08 13:44:03 -0700

    add pull-to-refresh functionality

--- app/src/main/java/com/codepath/instagramphotoviewer/activity/PhotosActivity.java
+++ app/src/main/java/com/codepath/instagramphotoviewer/activity/PhotosActivity.java
@@ -2,6 +2,7 @@ package com.codepath.instagramphotoviewer.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,11 +27,13 @@ public class PhotosActivity extends Activity {
     private static final String CLIENT_ID = "bc6c997f0da6402f927e7595180c5a83";
     private List<Photo> photos;
     private PhotosAdapter aPhotos;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+        setupSwitchRefreshLayout();
         photos = new ArrayList<>();
         aPhotos = new PhotosAdapter(this, photos);
         ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
@@ -38,16 +41,31 @@ public class PhotosActivity extends Activity {
         fetchPopularPhotos();
     }
 
+    private void setupSwitchRefreshLayout() {
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                fetchPopularPhotos();
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+    }
+
+
     private void fetchPopularPhotos() {
         String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, null, new JsonHttpResponseHandler() {
-            // onSuccess
-
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Log.i("DEBUG", response.toString());
+                aPhotos.clear();
                 ObjectMapper mapper = new ObjectMapper();
                 try {
                     JSONArray photosJson = response.getJSONArray("data");
@@ -59,10 +77,8 @@ public class PhotosActivity extends Activity {
                 }
 
                 aPhotos.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
             }
-
-
-            // onFailure
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
